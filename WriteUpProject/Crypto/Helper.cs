57b41218e96da417d52de6dd491f73a370ae0184
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-07-24 16:41:28 +0200

    skeleton

---
+++ WriteUpProject/Crypto/Helper.cs
@@ -0,0 +1,58 @@
+﻿using NBitcoin;
+using System;
+using System.Net;
+
+namespace WriteUpProject.Crypto
+{
+    public static class Helper
+    {
+        public static Network[] SupportedNetworks =
+        {
+            Network.Main,
+            Network.TestNet,
+            Network.TestNet4,
+            Network.RegTest
+        };
+
+        public static bool ValidateMessageBytesLength(byte[] messageBytes)
+        {
+            int maximumAllowedBytesForMessage = 80;
+
+            if (messageBytes.Length > maximumAllowedBytesForMessage) 
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static BitcoinAddress GetAddressFromString(string address, Network network) 
+        {
+            return BitcoinAddress.Create(address, network);
+        }
+
+        public static string BuildTx(Network network, byte[] messageBytes, string fundingTxID, uint vout, int amountSats,BitcoinAddress fundAddress, BitcoinAddress changeAddress, int fee)
+        {
+            Script opReturnScript = TxNullDataTemplate.Instance.GenerateScriptPubKey(messageBytes);
+            TxOut opReturnOutput = new(Money.Zero, opReturnScript);
+
+            Money inputAmount = new Money(amountSats, MoneyUnit.Satoshi);
+            OutPoint outpointOfFund = new OutPoint(uint256.Parse(fundingTxID), vout);
+            TxOut utxo = new TxOut(inputAmount, fundAddress);
+            TxIn txIn = new TxIn(outpointOfFund);
+            Coin coin = new Coin(outpointOfFund, utxo);
+
+            Money txFee = new Money(fee, MoneyUnit.Satoshi);
+            Money changeAmount = inputAmount - txFee;
+            TxOut changeOutput = new TxOut(changeAmount, changeAddress);
+
+            // Build TX
+            Transaction tx = Transaction.Create(network);
+            tx.Inputs.Add(txIn);
+            tx.Outputs.Add(opReturnOutput);
+            tx.Outputs.Add(changeOutput);
+            
+            return tx.ToHex();
+        }
+    }
+}
--- WriteUpProject/Views/MainView.axaml.cs
+++ WriteUpProject/Views/MainView.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using NBitcoin;
+using WriteUpProject.Crypto;
 
 namespace WriteUpProject.Views;
 
@@ -7,5 +9,35 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        int networkOptionIndex = NetworkCombobox.SelectedIndex;
+        Network network = Helper.SupportedNetworks[networkOptionIndex];
+
+        string message = MessageBox.Text ?? "";
+        byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+        if (message is null || !Helper.ValidateMessageBytesLength(messageBytes)) 
+        { 
+            // show error
+        }
+
+        string fundingTxID = TxIdBox.Text ?? "";
+        uint vout = uint.Parse(VoutBox.Text ?? "");
+        int amountSats = int.Parse(AmountBox.Text ?? "");
+        string fundAddressStr = FundAddressBox.Text ?? "";
+        BitcoinAddress fundAddress = Helper.GetAddressFromString(fundAddressStr, network);
+        string changeAddressStr = ChangeAddressBox.Text ?? "";
+        BitcoinAddress changeAddress = Helper.GetAddressFromString(changeAddressStr, network);
+
+        int fee = 100;
+        if (int.TryParse(FeeBox.Text, out var userFee)) 
+        {
+            fee = userFee;
+        }
+
+        ResultHexBox.Text = Helper.BuildTx(network, messageBytes, fundingTxID, vout, amountSats, fundAddress, changeAddress, fee);
     }
 }
