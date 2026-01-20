9f2582bdefe19d06cc0db20db59e88b280a6b877
adamPetho <45069029+adamPetho@users.noreply.github.com>
2026-01-20 18:18:40 +0100

    add Tx Preview page

---
+++ WriteUpProject/Views/Pages/TxPreviewPageView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+
+namespace WriteUpProject.Views.Pages;
+
+public partial class TxPreviewPageView : UserControl
+{
+    public TxPreviewPageView()
+    {
+        InitializeComponent();
+    }
+}
